c45841206f83bf9ed325470e14aa212efd64a011
nation-blue <429115937@qq.com>
2020-04-14 19:25:26 +0800

    submit

---
+++ week01/url.js
@@ -0,0 +1,11 @@
+
+// 解析URL地址
+function getParams(name) {
+	var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) {
+		return decodeURIComponent(r[2]);
+	}
+	return null;
+}
+
